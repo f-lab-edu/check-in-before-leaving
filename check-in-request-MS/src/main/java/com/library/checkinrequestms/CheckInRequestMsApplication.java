30d664d3bc5ca6b8fcefe4ebbbbe591fddcfab01
ScottSung7 <sungscott@naver.com>
2024-07-30 09:02:39 +0900

    [008-Kakao-Map-Rest-Api] Rest API 연동 확인 시작

---
+++ check-in-request-MS/src/main/java/com/library/checkinrequestms/CheckInRequestMsApplication.java
@@ -0,0 +1,13 @@
+package com.library.checkinrequestms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckInRequestMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CheckInRequestMsApplication.class, args);
+    }
+
+}
---
+++ check-in-request-MS/src/test/java/com/library/checkinrequestms/CheckInRequestMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.library.checkinrequestms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckInRequestMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
