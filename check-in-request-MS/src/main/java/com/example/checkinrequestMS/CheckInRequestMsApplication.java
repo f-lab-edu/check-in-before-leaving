7d05c7476b48bd17fa01cdb655f9d4d39c971264
ScottSung7 <sungscott@naver.com>
2024-08-11 11:04:44 +0900

    [011-모니터링] 빠진 파일

--- check-in-request-MS/src/main/java/com/example/checkinrequestMS/CheckInRequestMsApplication.java
+++ check-in-request-MS/src/main/java/com/example/checkinrequestMS/CheckInRequestMsApplication.java
@@ -2,7 +2,9 @@ package com.example.checkinrequestMS;
 
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
@@ -10,6 +12,11 @@ public class CheckInRequestMsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CheckInRequestMsApplication.class, args);
+    }
+
+    @Bean
+    public InMemoryHttpExchangeRepository inMemoryHttpExchangeRepository() {
+        return new InMemoryHttpExchangeRepository();
     }
 
 }
