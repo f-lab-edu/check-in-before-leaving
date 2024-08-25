7562790c07156845ae240ddec82876faff337267
ScottSung7 <sungscott@naver.com>
2024-08-26 04:21:23 +0900

    [020-Place-Redis로-DB-변경] Redis와 Redis GEO를 이용한 Place 검색 및 저장

--- check-in-request-MS/src/main/java/com/example/checkinrequestMS/PlaceAPI/domain/service/tools/KakaoAPIStoreInfoSaver.java
+++ check-in-request-MS/src/main/java/com/example/checkinrequestMS/PlaceAPI/domain/service/tools/KakaoAPIStoreInfoSaver.java
@@ -1,8 +1,12 @@
 package com.example.checkinrequestMS.PlaceAPI.domain.service.tools;
 
 import com.example.checkinrequestMS.PlaceAPI.domain.Place;
+import com.example.checkinrequestMS.PlaceAPI.domain.PlaceRedis;
 import com.example.checkinrequestMS.PlaceAPI.infra.PlaceJPARepository;
+import com.example.checkinrequestMS.PlaceAPI.infra.redis.PlaceRedisRepository;
+import com.example.checkinrequestMS.PlaceAPI.infra.redis.RedisGeo;
 import com.example.checkinrequestMS.PlaceAPI.web.restAPI.KakaoStoreAPIRequest;
+import com.example.checkinrequestMS.PlaceAPI.web.restAPI.SearchCategory;
 import com.example.checkinrequestMS.PlaceAPI.web.restAPI.SearchType;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
@@ -14,43 +18,73 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static com.example.checkinrequestMS.PlaceAPI.web.restAPI.SearchType.CATEGORY;
 import static com.example.checkinrequestMS.PlaceAPI.web.restAPI.SearchType.KEYWORD;
 
 @Component
 @RequiredArgsConstructor
 public class KakaoAPIStoreInfoSaver {
 
-    private final PlaceJPARepository storeRepository;
+    private final PlaceRedisRepository placeRedisRepository;
+    private final PlaceJPARepository placeJPARepository;
     private final KakaoStoreAPIRequest kakaoAPIRequest;
+    private final RedisGeo redisGeo;
 
-    //SAVE TO DB : DB에 바로 저장.
-    public void saveToDBWithCategory(String query, double x, double y, int radius){
-        saveStoreInfoToDB(SearchType.CATEGORY, query, x, y, radius);
+    //SAVE TO REDIS : Redis에 저장.
+    public void saveToRedisWithKeyWord(String query, double x, double y, int radius){
+        saveStoreInfoToRedis(SearchType.KEYWORD, query, x, y, radius);
     }
+    public void saveToRedisWithCategory(SearchCategory category, double x, double y, int radius){
+        saveStoreInfoToRedis(SearchType.CATEGORY, category.code(), x, y, radius);
+    }
+    private void saveStoreInfoToRedis(SearchType type, String query, double x, double y, int radius){
+        String response = kakaoAPIRequest.getStoreInfo(type, query, x, y, radius);
+        List<Place> list = parsePlaceInfo(response);
+
+        if (list.isEmpty()) return;
+        for (Place place : list) {
+            placeRedisRepository.save(PlaceRedis.from(place));
+            redisGeo.addLocation(place.getId(), place.getX(), place.getY());
+        }
+    }
+
+    // DB에 저장
+    // 주의: deprecated, Save to Redis 사용.
     public void saveToDBWithKeyword(String query, double x, double y, int radius){
         saveStoreInfoToDB(SearchType.KEYWORD, query, x, y, radius);
+    }
+    public void saveToDBWithCategory(SearchCategory category, double x, double y, int radius){
+        saveStoreInfoToDB(SearchType.CATEGORY, category.code(), x, y, radius);
     }
     private void saveStoreInfoToDB(SearchType type, String query, double x, double y, int radius){
         String response = kakaoAPIRequest.getStoreInfo(type, query, x, y, radius);
         List<Place> list = parsePlaceInfo(response);
 
         if (list.isEmpty()) return;
-        for (Place place : list) storeRepository.save(place);
+        placeJPARepository.saveAll(list);
     }
 
-    //BALANCE : 이전에 사용하던 DB 내용과 비교 후 저장.
+    // 비교 저장 - 카카오 API 검색 결과와 내부 DB 비교 후 달라진 내용 업데이트.
+    // 주의: deprecated, Save to Redis 사용.
     //check: 이후 다양한 쿼리에서 저장 가능하게 하도록 하려고 합니다. **현재 getStoresByNameAndRadius도 이름은 뺴고 좌표의 범위로만 검색합니다. 이후 수정하겠습니다!
-    public void balanceKeyWordSearch(String query, double x, double y, int radius){
-        String response = kakaoAPIRequest.getStoreInfo(KEYWORD, query, x, y, radius);
+    public void balanceAndSaveWithKeyword(String query, double x, double y, int radius){
+        saveStoreInfoToDB_Balance(KEYWORD, query, x, y, radius);
+    }
+    public void balanceAndSaveWithCategory(SearchCategory category, double x, double y, int radius){
+        saveStoreInfoToDB_Balance(CATEGORY, category.code(), x, y, radius);
+    }
+    private void saveStoreInfoToDB_Balance(SearchType type, String query, double x, double y, int radius){
+        String response = kakaoAPIRequest.getStoreInfo(type, query, x, y, radius);
 
         List<Place> placesFromAPI = parsePlaceInfo(response); //Brute Force 이후 범위를 경도/위도 조합을 스캔하며 저장해서 끝난 부분은 다시 검색 안해도 됨.
-        List<Place> placesFromDB = storeRepository.getStoresByNameAndRadius(x, y, radius).get();
+        List<Place> placesFromDB = placeJPARepository.getStoresByNameAndRadius(x, y, radius).get();
         List<Place> list = placesFromAPI.stream().filter(place -> placesFromDB.stream().noneMatch(a -> place.equals(a))).collect(Collectors.toList());
 
         if (list.isEmpty()) return;
-        for (Place place : list) storeRepository.save(place);
-
+        placeJPARepository.saveAll(list);
     }
+
+
     private List<Place> parsePlaceInfo(String response){
         try {
             ObjectMapper objectMapper = new ObjectMapper();
--- check-in-request-MS/src/test/java/com/example/checkinrequestMS/PlaceAPI/domain/service/tools/KakaoAPIStoreInfoSaverTest.java
+++ check-in-request-MS/src/test/java/com/example/checkinrequestMS/PlaceAPI/domain/service/tools/KakaoAPIStoreInfoSaverTest.java
@@ -2,7 +2,10 @@ package com.example.checkinrequestMS.PlaceAPI.domain.service.tools;
 
 import com.example.checkinrequestMS.PlaceAPI.domain.Place;
 import com.example.checkinrequestMS.PlaceAPI.infra.PlaceJPARepository;
+import com.example.checkinrequestMS.PlaceAPI.infra.redis.PlaceRedisRepository;
+import com.example.checkinrequestMS.PlaceAPI.infra.redis.RedisGeo;
 import com.example.checkinrequestMS.PlaceAPI.web.restAPI.KakaoStoreAPIRequest;
+import com.example.checkinrequestMS.PlaceAPI.web.restAPI.SearchCategory;
 import com.example.checkinrequestMS.PlaceAPI.web.restAPI.SearchType;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -16,6 +19,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
+import static com.example.checkinrequestMS.PlaceAPI.web.restAPI.SearchCategory.RESTAURANT;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.*;
@@ -27,10 +31,16 @@ class KakaoAPIStoreInfoSaverTest {
     KakaoAPIStoreInfoSaver sut;
 
     @Mock
-    private PlaceJPARepository storeRepository;
+    private PlaceJPARepository placeJPARepository;
 
     @Mock
     private KakaoStoreAPIRequest kakaoAPIRequest;
+
+    @Mock
+    private PlaceRedisRepository placeRedisRepository;
+
+    @Mock
+    private RedisGeo redisGeo;
 
     private String response;
 
@@ -38,44 +48,121 @@ class KakaoAPIStoreInfoSaverTest {
     void setUp() {
         response =
                 "[{\"address_name\":\"서울 중구 명동2가 25-36\",\"category_group_code\":\"FD6\",\"category_group_name\":\"음식점\",\"category_name\":\"음식점 > 분식\",\"distance\":\"0\",\"id\":\"10332413\",\"phone\":\"02-776-5348\",\"place_name\":\"명동교자 본점\",\"place_url\":\"http://place.map.kakao.com/10332413\",\"road_address_name\":\"서울 중구 명동10길 29\",\"x\":\"126.98561429978552\",\"y\":\"37.56255453417897\"}," +
-                 "{\"address_name\":\"서울 중구 명동2가 4-2\",\"category_group_code\":\"FD6\",\"category_group_name\":\"음식점\",\"category_name\":\"음식점 > 한식\",\"distance\":\"42\",\"id\":\"26853115\",\"phone\":\"02-3789-9292\",\"place_name\":\"순남시래기 명동직영점\",\"place_url\":\"http://place.map.kakao.com/26853115\",\"road_address_name\":\"서울 중구 명동10길 35-20\",\"x\":\"126.985784007806\",\"y\":\"37.5629131513515\"}]";
+                        "{\"address_name\":\"서울 중구 명동2가 4-2\",\"category_group_code\":\"FD6\",\"category_group_name\":\"음식점\",\"category_name\":\"음식점 > 한식\",\"distance\":\"42\",\"id\":\"26853115\",\"phone\":\"02-3789-9292\",\"place_name\":\"순남시래기 명동직영점\",\"place_url\":\"http://place.map.kakao.com/26853115\",\"road_address_name\":\"서울 중구 명동10길 35-20\",\"x\":\"126.985784007806\",\"y\":\"37.5629131513515\"}]";
     }
 
     @Test
-    @DisplayName("API에서 받은 정보 모두 저장")
-    void add_all_from_API() {
+    @DisplayName("Redis 저장 - 키워드 저장 성공")
+    void saveToRedisWithKeyWord() {
+        //given
+        String query = "맛집";
+        double x = 126.98561429978552;
+        double y = 37.56255453417897;
+        int radius = 50;
+        given(kakaoAPIRequest.getStoreInfo(any(SearchType.class), anyString(), anyDouble(), anyDouble(), anyInt())).willReturn(response);
+
+        //when
+        sut.saveToRedisWithKeyWord(query, x, y, radius);
+
+        //then
+        verify(kakaoAPIRequest).getStoreInfo(SearchType.KEYWORD, "맛집", x, y, radius);
+        verify(placeRedisRepository, times(2)).save(any());
+        verify(redisGeo, times(2)).addLocation(anyLong(), anyDouble(), anyDouble());
+    }
+
+    @Test
+    @DisplayName("Redis 저장 - 카테고리 저장 성공")
+    void saveToRedisWithCategory() {
+        //given
+        double x = 126.98561429978552;
+        double y = 37.56255453417897;
+        int radius = 50;
+        given(kakaoAPIRequest.getStoreInfo(any(SearchType.class), anyString(), anyDouble(), anyDouble(), anyInt())).willReturn(response);
+
+        //when
+        sut.saveToRedisWithCategory(RESTAURANT, x, y, radius);
+
+        //then
+        verify(kakaoAPIRequest).getStoreInfo(SearchType.CATEGORY, RESTAURANT.code(), x, y, radius);
+        verify(placeRedisRepository, times(2)).save(any());
+        verify(redisGeo, times(2)).addLocation(anyLong(), anyDouble(), anyDouble());
+    }
+
+
+    // DB에 저장 - 주의: deprecated, Save to Redis 사용.
+    @Test
+    @DisplayName("DB 저장 - 키워드 저장 성공")
+    void saveToDBWithKeyword() {
+        //given
+        String query = "맛집";
+        double x = 126.98561429978552;
+        double y = 37.56255453417897;
+        int radius = 50;
+        given(kakaoAPIRequest.getStoreInfo(any(SearchType.class), anyString(), anyDouble(), anyDouble(), anyInt())).willReturn(response);
+
+        //when
+        sut.saveToDBWithKeyword(query, x, y, radius);
+
+        //then
+        verify(kakaoAPIRequest).getStoreInfo(SearchType.KEYWORD, "맛집", x, y, radius);
+        verify(placeJPARepository, times(1)).saveAll(anyList());
+    }
+
+    @Test
+    @DisplayName("DB 저장 - 카테고리 저장 성공")
+    void saveToDBWithCategory() {
+        //given
+        double x = 126.98561429978552;
+        double y = 37.56255453417897;
+        int radius = 50;
+        given(kakaoAPIRequest.getStoreInfo(any(SearchType.class), anyString(), anyDouble(), anyDouble(), anyInt())).willReturn(response);
+
+        //when
+        sut.saveToDBWithCategory(RESTAURANT, x, y, radius);
+
+        //then
+        verify(kakaoAPIRequest).getStoreInfo(SearchType.CATEGORY, RESTAURANT.code(), x, y, radius);
+        verify(placeJPARepository, times(1)).saveAll(anyList());
+    }
+
+    // 비교저장 - 주의: deprecated, Save to Redis 사용.
+    @Test
+    @DisplayName("비교 저장 - 키워드 검색, API에서 받은 정보 모두 저장")
+    void balanceAndSaveWithKeyword() {
         //given
         List<Place> placesFromDB = mock();
 
         given(kakaoAPIRequest.getStoreInfo(any(SearchType.class), anyString(), anyDouble(), anyDouble(), anyInt())).willReturn(response);
-        given(storeRepository.getStoresByNameAndRadius(anyDouble(), anyDouble(), anyInt())).willReturn(Optional.of(placesFromDB));
+        given(placeJPARepository.getStoresByNameAndRadius(anyDouble(), anyDouble(), anyInt())).willReturn(Optional.of(placesFromDB));
 
         //when
-        sut.balanceKeyWordSearch("맛집", 126.98561429978552, 37.56255453417897, 50);
+        sut.balanceAndSaveWithKeyword("맛집", 126.98561429978552, 37.56255453417897, 50);
 
         //then
-        verify(storeRepository, times(2)).save(any(Place.class));
+        verify(placeJPARepository, times(1)).saveAll(anyList());
     }
+
     @Test
-    @DisplayName("API에서 받은 정보 중 1개 없어서 저장")
-    void add_only_one_from_API() {
+    @DisplayName("비교 저장 - 키워드 검색, API에서 받은 정보 중 1개 없어서 저장")
+    void balanceAndSaveWithKeyword_addOnlyOneFromAPI() {
         //given
         Place place1 = mock();
         given(place1.getPlaceName()).willReturn("명동교자 본점");
         List<Place> placesFromDB = spy(Arrays.asList(place1));
 
         given(kakaoAPIRequest.getStoreInfo(any(SearchType.class), anyString(), anyDouble(), anyDouble(), anyInt())).willReturn(response);
-        given(storeRepository.getStoresByNameAndRadius(anyDouble(), anyDouble(), anyInt())).willReturn(Optional.of(placesFromDB));
+        given(placeJPARepository.getStoresByNameAndRadius(anyDouble(), anyDouble(), anyInt())).willReturn(Optional.of(placesFromDB));
 
         //when
-        sut.balanceKeyWordSearch("맛집", 126.98561429978552, 37.56255453417897, 50);
+        sut.balanceAndSaveWithKeyword("맛집", 126.98561429978552, 37.56255453417897, 50);
 
         //then
-        verify(storeRepository, times(1)).save(any(Place.class));
+        verify(placeJPARepository, times(1)).saveAll(anyList());
     }
+
     @Test
-    @DisplayName("API에서 받은 정보가 DB에 이미 있는 경우 저장하지 않음")
-    void add_none_from_API(){
+    @DisplayName("비교 저장 - 키워드 검색, API에서 받은 정보가 DB에 이미 있는 경우 저장하지 않음")
+    void balanceAndSaveWithKeyword_addNoneFromAPI() {
         //given
         Place place1 = mock();
         given(place1.getPlaceName()).willReturn("명동교자 본점");
@@ -84,12 +171,66 @@ class KakaoAPIStoreInfoSaverTest {
         List<Place> placesFromDB = spy(Arrays.asList(place1, place2));
 
         given(kakaoAPIRequest.getStoreInfo(any(SearchType.class), anyString(), anyDouble(), anyDouble(), anyInt())).willReturn(response);
-        given(storeRepository.getStoresByNameAndRadius(anyDouble(), anyDouble(), anyInt())).willReturn(Optional.of(placesFromDB));
+        given(placeJPARepository.getStoresByNameAndRadius(anyDouble(), anyDouble(), anyInt())).willReturn(Optional.of(placesFromDB));
 
         //when
-        sut.balanceKeyWordSearch("맛집", 126.98561429978552, 37.56255453417897, 50);
+        sut.balanceAndSaveWithKeyword("맛집", 126.98561429978552, 37.56255453417897, 50);
 
         //then
-        verify(storeRepository, times(0)).save(any(Place.class));
+        verify(placeJPARepository, times(0)).save(any(Place.class));
+    }
+
+    @Test
+    @DisplayName("비교 저장 - 카테고리 검색, API에서 받은 정보 모두 저장")
+    void balanceAndSaveWithCategory() {
+        //given
+        List<Place> placesFromDB = mock();
+
+        given(kakaoAPIRequest.getStoreInfo(any(SearchType.class), anyString(), anyDouble(), anyDouble(), anyInt())).willReturn(response);
+        given(placeJPARepository.getStoresByNameAndRadius(anyDouble(), anyDouble(), anyInt())).willReturn(Optional.of(placesFromDB));
+
+        //when
+        sut.balanceAndSaveWithCategory(RESTAURANT, 126.98561429978552, 37.56255453417897, 50);
+
+        //then
+        verify(placeJPARepository, times(1)).saveAll(anyList());
+    }
+
+    @Test
+    @DisplayName("비교 저장 - 카테고리 검색, API에서 받은 정보 중 1개 없어서 저장")
+    void balanceAndSaveWithCategory_addOnlyOneFromAPI() {
+        //given
+        Place place1 = mock();
+        given(place1.getPlaceName()).willReturn("명동교자 본점");
+        List<Place> placesFromDB = spy(Arrays.asList(place1));
+
+        given(kakaoAPIRequest.getStoreInfo(any(SearchType.class), anyString(), anyDouble(), anyDouble(), anyInt())).willReturn(response);
+        given(placeJPARepository.getStoresByNameAndRadius(anyDouble(), anyDouble(), anyInt())).willReturn(Optional.of(placesFromDB));
+
+        //when
+        sut.balanceAndSaveWithCategory(RESTAURANT, 126.98561429978552, 37.56255453417897, 50);
+
+        //then
+        verify(placeJPARepository, times(1)).saveAll(anyList());
+    }
+
+    @Test
+    @DisplayName("비교 저장 - 카테고리 검색, API에서 받은 정보가 DB에 이미 있는 경우 저장하지 않음")
+    void balanceAndSaveWithCategory_addNoneFromAPI() {
+        //given
+        Place place1 = mock();
+        given(place1.getPlaceName()).willReturn("명동교자 본점");
+        Place place2 = mock();
+        given(place2.getPlaceName()).willReturn("순남시래기 명동직영점");
+        List<Place> placesFromDB = spy(Arrays.asList(place1, place2));
+
+        given(kakaoAPIRequest.getStoreInfo(any(SearchType.class), anyString(), anyDouble(), anyDouble(), anyInt())).willReturn(response);
+        given(placeJPARepository.getStoresByNameAndRadius(anyDouble(), anyDouble(), anyInt())).willReturn(Optional.of(placesFromDB));
+
+        //when
+        sut.balanceAndSaveWithCategory(RESTAURANT, 126.98561429978552, 37.56255453417897, 50);
+
+        //then
+        verify(placeJPARepository, times(0)).save(any(Place.class));
     }
 }
