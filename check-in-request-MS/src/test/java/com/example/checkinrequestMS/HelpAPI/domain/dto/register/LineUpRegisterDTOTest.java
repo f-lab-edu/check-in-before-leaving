d93a0148fd69b2b7b8d8bb92c4c382e98e40cb1b
ScottSung7 <sungscott@naver.com>
2024-09-07 12:54:10 +0900

    [025-1-Help-Progress-구조변경 테스트 줄서기] 줄서기 테스트 추가 내역

---
+++ check-in-request-MS/src/test/java/com/example/checkinrequestMS/HelpAPI/domain/dto/register/LineUpRegisterDTOTest.java
@@ -0,0 +1,29 @@
+package com.example.checkinrequestMS.HelpAPI.domain.dto.register;
+
+import com.example.checkinrequestMS.HelpAPI.domain.dto.write.register.child.LineUpRegisterDTO;
+import com.example.checkinrequestMS.HelpAPI.web.dto.form.help.write.register.child.LineUpRegisterForm;
+import com.example.checkinrequestMS.fixtures.HelpAPI.web.form.help.register.LineUpRegisterFormFixture;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class LineUpRegisterDTOTest {
+
+    @Test
+    void from() {
+        //given
+        LineUpRegisterForm form = LineUpRegisterFormFixture.create();
+
+        //when
+        LineUpRegisterDTO sut = LineUpRegisterDTO.from(form);
+
+        //then
+        assertEquals(form.getHelpRegisterId(), sut.getHelpRegisterId());
+        assertEquals(form.getPlaceId(), sut.getPlaceId());
+        assertEquals(form.getStart(), sut.getStart());
+        assertEquals(form.getStart().plusHours(form.getOption()), sut.getEnd());
+        assertEquals(form.getReward(), sut.getReward());
+
+    }
+
+}
