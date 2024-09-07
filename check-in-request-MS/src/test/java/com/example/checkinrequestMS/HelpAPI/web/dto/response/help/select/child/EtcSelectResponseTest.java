f6c31a369049f488ae84dea09d23f1bc2ef33ac5
ScottSung7 <sungscott@naver.com>
2024-09-07 13:08:53 +0900

    [025-2-Help-Progress-구조-변경-Etc] Etc 테스트 추가

---
+++ check-in-request-MS/src/test/java/com/example/checkinrequestMS/HelpAPI/web/dto/response/help/select/child/EtcSelectResponseTest.java
@@ -0,0 +1,31 @@
+package com.example.checkinrequestMS.HelpAPI.web.dto.response.help.select.child;
+
+import com.example.checkinrequestMS.HelpAPI.domain.model.help.ProgressVO.Created;
+import com.example.checkinrequestMS.HelpAPI.domain.model.help.child.Etc;
+import com.example.checkinrequestMS.HelpAPI.web.dto.response.progress.read.ProgressSelectResponse;
+import com.example.checkinrequestMS.fixtures.HelpAPI.domain.model.help.child.EtcFixture;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EtcSelectResponseTest {
+
+    @Test
+    void from() {
+        //given
+        Etc<Created> etc = EtcFixture.createEtcWithId(Created.create(), 1L);
+        //when
+        EtcSelectResponse sut = EtcSelectResponse.from(etc);
+
+        //then
+        assertEquals(sut.getId(), etc.getId());
+        assertEquals(sut.getHelpRegisterId(), etc.getHelpRegisterId());
+        assertEquals(sut.getTitle(), etc.getTitle());
+        assertEquals(sut.getPlaceId(), etc.getPlaceId());
+        assertEquals(sut.getStart(), etc.getStart());
+        assertEquals(sut.getEnd(), etc.getEnd());
+        assertEquals(sut.getReward(), etc.getReward());
+        assertEquals(sut.getProgressDTO().getClass(), ProgressSelectResponse.getProgressDTO(etc.getProgress()).getClass());
+        assertEquals(sut.getContents(), etc.getContents());
+    }
+}
