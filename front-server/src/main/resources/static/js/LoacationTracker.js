db6f3fe3b77a44f0173d5e4689999a70dc31a244
ScottSung7 <sungscott@naver.com>
2024-09-25 15:57:17 +0900

    [030-Front-Server-테스트-요청 추가] Location 업데이트 및 로그인 테스트를 위한 변경

--- front-server/src/main/resources/static/js/LoacationTracker.js
+++ front-server/src/main/resources/static/js/LoacationTracker.js
@@ -36,12 +36,13 @@ class LocationTracker {
     sendLocationToServer(locationData) {
         fetch(this.apiUrl, {
             method: 'POST',
+            credentials: 'include',
             headers: {
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify(locationData)
         })
-            .then(response => response.json())
+            .then(response => response.text())
             .then(data => {
                 console.log('Location sent successfully:', data);
             })
@@ -53,3 +54,54 @@ class LocationTracker {
 
 const tracker = new LocationTracker('http:///localhost:8080/track');
 tracker.start(); // 추적 시작
+
+function hello() {
+    console.log("click");
+    const signUpData = {
+        email: 'example@example.com',
+        password: '1234',
+        name: 'Your Name',
+        phone: '010-1234-5678',
+        location: 'Seoul',
+        isLocationServiceEnabled: true,
+        point: 100
+    };
+
+    fetch('http://localhost:8080/sign-in', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(signUpData)
+    })
+        .then(response => response.text())
+        .then(data => {
+            console.log('Success:', data);
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+}
+
+function login() {
+    const LoginData = {
+        email: 'example@example.com',
+        password: '1234',
+    };
+
+    fetch('http://localhost:8080/log-in', {
+        method: 'POST',
+        credentials: 'include',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(LoginData)
+    })
+        .then(response => response.text())
+        .then(data => {
+            console.log('Success:', data);
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+}
