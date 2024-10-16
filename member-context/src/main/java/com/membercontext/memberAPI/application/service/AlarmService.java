cdd387eabe48ccc73d7b77ba93b3a2893b2eddc4
ScottSung7 <sungscott@naver.com>
2024-10-16 18:15:01 +0900

    [038-Member-테스트-재정비-서비스,도메인계층] 서비스,도메인 변경 부분

--- member-context/src/main/java/com/membercontext/memberAPI/application/service/AlarmService.java
+++ member-context/src/main/java/com/membercontext/memberAPI/application/service/AlarmService.java
@@ -1,9 +1,7 @@
 package com.membercontext.memberAPI.application.service;
 
-import com.membercontext.memberAPI.application.exception.alarm.AlarmException;
-import com.membercontext.memberAPI.application.exception.member.MemberException;
+import com.membercontext.memberAPI.application.repository.MemberRepository;
 import com.membercontext.memberAPI.domain.entity.member.Member;
-import com.membercontext.memberAPI.infrastructure.db.jpa.member.MemberJPARepository;
 import com.membercontext.memberAPI.web.controller.AlarmController;
 import com.membercontext.memberAPI.web.pushMessage.FireBaseCloudMessageClient;
 import lombok.RequiredArgsConstructor;
@@ -11,19 +9,17 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-import static com.membercontext.memberAPI.application.exception.alarm.AlarmErrorCode.PUSH_ALARM_FAILED;
-
 @Service
 @RequiredArgsConstructor
 public class AlarmService {
 
-    private final MemberJPARepository memberJPARepository;
+    private final MemberRepository memberRepository;
     private final FireBaseCloudMessageClient fireBaseCloudMessageClient;
 
     private static final int radius = 40;
 
-    public void sendPushMessage(AlarmController.AlarmForm alarmForm) {
-        List<Member> membersToPush = memberJPARepository.findNearByMember(alarmForm.getX(), alarmForm.getY(), radius);
+    public void sendPushMessage(AlarmController.AlarmRequest alarmForm) {
+        List<Member> membersToPush = memberRepository.findNearByMember(alarmForm.getX(), alarmForm.getY(), radius);
         if (membersToPush.isEmpty()) {
             return;
         }
