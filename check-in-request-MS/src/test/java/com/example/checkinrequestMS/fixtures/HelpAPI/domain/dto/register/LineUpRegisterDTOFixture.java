d0694fcc2ec0438ec9413a0498d62cc5a7298a50
ScottSung7 <sungscott@naver.com>
2024-10-29 21:52:09 +0900

    [043-Progress-구조-변경-영향받은-서비스들] LineUp 관련 테스트 변경사항

--- check-in-request-MS/src/test/java/com/example/checkinrequestMS/fixtures/HelpAPI/domain/dto/register/LineUpRegisterDTOFixture.java
+++ check-in-request-MS/src/test/java/com/example/checkinrequestMS/fixtures/HelpAPI/domain/dto/register/LineUpRegisterDTOFixture.java
@@ -3,17 +3,19 @@ package com.example.checkinrequestMS.fixtures.HelpAPI.domain.dto.register;
 import com.example.checkinrequestMS.HelpAPI.domain.dto.write.register.child.LineUpRegisterDTO;
 import com.example.checkinrequestMS.fixtures.Variables;
 
+import static com.example.checkinrequestMS.fixtures.Variables.*;
+
 public class LineUpRegisterDTOFixture extends LineUpRegisterDTO {
 
     public static LineUpRegisterDTO create() {
         return LineUpRegisterDTO.builder()
-                .helpRegisterId(1L)
-                .placeId(1L)
-                .start(Variables.startTime)
-                .end(Variables.startTime.plusHours(1L))
-                .reward(100L)
+                .helpRegisterId(HELP_REGISTER_ID)
+                .placeId(PLACE_ID)
+                .start(START_TIME)
+                .end(START_TIME.plusHours(ONE_HOUR))
+                .reward(REWARD)
                 .build();
-            //todo: hour, minute 차이 강제 방법. (?)
+        //todo: hour, minute 차이 강제 방법. (?)
 
     }
 }
