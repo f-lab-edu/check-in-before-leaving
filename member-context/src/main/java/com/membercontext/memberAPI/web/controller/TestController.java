2efb338a7fdf43eac4bc51da1d3af6700014848b
ScottSung7 <sungscott@naver.com>
2024-11-01 16:45:29 +0900

    [HOTFIX-05-인증-AOP-정상화] AOP 정상동작 확인 + 컨트롤러 어노테이션 변경

--- member-context/src/main/java/com/membercontext/memberAPI/web/controller/TestController.java
+++ member-context/src/main/java/com/membercontext/memberAPI/web/controller/TestController.java
@@ -1,20 +1,25 @@
 package com.membercontext.memberAPI.web.controller;
 
+import com.membercontext.memberAPI.application.aop.authentication.NoAuthentication;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/test")
 public class TestController {
 
     @PostMapping
+    @NoAuthentication
     public void test() {
         System.out.println("test Post on going");
     }
+
     @GetMapping
+    @NoAuthentication
     public void test2() {
         System.out.println("test Get on going");
     }
