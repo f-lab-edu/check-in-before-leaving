683c0b0c7cef2b1bfa7110310d7aa33d2b29ce42
ScottSung7 <sungscott@naver.com>
2024-11-02 21:59:43 +0900

    [48-Member-도메인-영역-캡슐화] 기타 질문

--- member-context/src/main/java/com/membercontext/memberAPI/application/service/MemberInfo/MemberInfoServiceImpl.java
+++ member-context/src/main/java/com/membercontext/memberAPI/application/service/MemberInfo/MemberInfoServiceImpl.java
@@ -17,9 +17,11 @@ public class MemberInfoServiceImpl implements MemberInfoService {
     @Override
     public Member getMemberInfo(String id) {
         Member member = memberRepository.findById(id);
+        //fixme: 조회에 대한 책임도 응용계층에서 처리하게 되는지 궁금합니다. 이 로직을 Member에 넣기도 어려울 것 같아서요.
         if (member == null) {
             throw new MemberException(NOT_EXITING_USER);
         }
         return member;
+
     }
 }
