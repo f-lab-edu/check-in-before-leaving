51f0a6a89de928c2aefd29316b715d3e1fce5b37
ScottSung7 <sungscott@naver.com>
2024-11-16 09:52:51 +0900

    [HOTFIX-11-5-Check-In-MS-표현계층-형식변경] Help Select 의 instance of 해결 관련 변경 파일

--- check-in-request-MS/src/main/java/com/example/checkinrequestMS/HelpAPI/web/controller/help/read/HelpSelectController.java
+++ check-in-request-MS/src/main/java/com/example/checkinrequestMS/HelpAPI/web/controller/help/read/HelpSelectController.java
@@ -1,12 +1,14 @@
 package com.example.checkinrequestMS.HelpAPI.web.controller.help.read;
 
+import com.example.checkinrequestMS.HelpAPI.application.exceptions.help.HelpException;
 import com.example.checkinrequestMS.HelpAPI.application.service.help.read.HelpSelectService;
 
 import com.example.checkinrequestMS.HelpAPI.domain.model.help.Help;
+import com.example.checkinrequestMS.HelpAPI.domain.model.help.Progress;
 import com.example.checkinrequestMS.HelpAPI.domain.model.help.child.CheckIn;
 import com.example.checkinrequestMS.HelpAPI.domain.model.help.child.Etc;
 import com.example.checkinrequestMS.HelpAPI.domain.model.help.child.LineUp;
-import com.example.checkinrequestMS.HelpAPI.web.controller.dto.response.progress.read.ProgressSelectResponse;
+import com.example.checkinrequestMS.HelpAPI.web.controller.progress.wrtie.ProgressWriteController;
 import lombok.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +17,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
+import java.util.List;
+
+import static com.example.checkinrequestMS.HelpAPI.application.exceptions.help.HelpErrorCode.HELP_RESPONSE_CREATION_ERROR;
 
 @RestController
 @RequestMapping("/help/")
@@ -24,11 +29,28 @@ public class HelpSelectController {
     private final HelpSelectService helpSelectService;
 
     @GetMapping("/{id}")
-    public ResponseEntity<HelpSelectResponse> selecHelp(@PathVariable Long id) {
-        return ResponseEntity.ok(HelpSelectResponse.from(helpSelectService.selectHelp(id)));
+    public ResponseEntity<HelpSelectResponse> selectHelp(@PathVariable Long id) {
+        return ResponseEntity.ok(HelpSelectResponseFactory.from(helpSelectService.selectHelp(id)));
     }
 
-    //Response
+    // Factory
+    public static class HelpSelectResponseFactory {
+        static final List<HelpSelectResponse> responses = List.of(
+                new CheckInSelectResponse(),
+                new EtcSelectResponse(),
+                new LineUpSelectResponse()
+        );
+
+        public static HelpSelectResponse from(Help help) {
+            return responses.stream()
+                    .filter(response -> response.canHandle(help))
+                    .findFirst()
+                    .map(response -> response.createResponse(help))
+                    .orElseThrow(() -> new HelpException(HELP_RESPONSE_CREATION_ERROR));
+        }
+    }
+
+    //Responses
     @Getter
     @NoArgsConstructor(access = AccessLevel.PROTECTED)
     @AllArgsConstructor(access = AccessLevel.PROTECTED)
@@ -40,31 +62,31 @@ public class HelpSelectController {
         protected String placeId;
         protected LocalDateTime start;
         protected LocalDateTime end;
-        protected ProgressSelectResponse progressSelectResponse;
+        protected ProgressWriteController.ProgressSelectResponse progressSelectResponse;
         protected Long reward;
 
-        public static HelpSelectResponse from(Help help) {
-            if (help instanceof CheckIn) {
-                return CheckInSelectResponse.from((CheckIn) help);
-            } else if (help instanceof LineUp) {
-                return LineUpSelectResponse.from((LineUp) help);
-            } else if (help instanceof Etc) {
-                return EtcSelectResponse.from((Etc) help);
-            }
-            throw new IllegalArgumentException("Unknown help type");
-        }
+        public abstract boolean canHandle(Help help);
+
+        public abstract HelpSelectResponse createResponse(Help help);
     }
 
     @Getter
     @NoArgsConstructor(access = AccessLevel.PROTECTED)
     public static class CheckInSelectResponse extends HelpSelectResponse {
 
-        @Builder(access = AccessLevel.PROTECTED)
-        private CheckInSelectResponse(Long id, Long helpRegisterId, String title, String placeId, LocalDateTime start, LocalDateTime end, ProgressSelectResponse progressSelectResponse, Long reward) {
+        @Builder(access = AccessLevel.PRIVATE)
+        private CheckInSelectResponse(Long id, Long helpRegisterId, String title, String placeId, LocalDateTime start, LocalDateTime end, ProgressWriteController.ProgressSelectResponse progressSelectResponse, Long reward) {
             super(id, helpRegisterId, title, placeId, start, end, progressSelectResponse, reward);
         }
 
-        public static CheckInSelectResponse from(CheckIn checkIn) {
+        @Override
+        public boolean canHandle(Help help) {
+            return help instanceof CheckIn;
+        }
+
+        @Override
+        public CheckInSelectResponse createResponse(Help help) {
+            CheckIn checkIn = (CheckIn) help;
             return CheckInSelectResponse.builder()
                     .id(checkIn.getId())
                     .helpRegisterId(checkIn.getHelpRegisterId())
@@ -73,7 +95,21 @@ public class HelpSelectController {
                     .start(checkIn.getStart())
                     .end(checkIn.getEnd())
                     .reward(checkIn.getReward())
-                    .progressSelectResponse(ProgressSelectResponse.getProgressSelectResponse(checkIn.getProgress()))
+                    .progressSelectResponse(ProgressWriteController.ProgressSelectResponse.getProgressSelectResponse(checkIn.getProgress()))
+                    .build();
+        }
+
+        //Test
+        public static CheckInSelectResponse createForTest() {
+            return CheckInSelectResponse.builder()
+                    .id(1L)
+                    .helpRegisterId(1L)
+                    .title("title")
+                    .placeId("placeId")
+                    .start(LocalDateTime.now())
+                    .end(LocalDateTime.now())
+                    .progressSelectResponse(ProgressWriteController.ProgressSelectResponse.getProgressSelectResponse(Progress.DEFAULT))
+                    .reward(100L)
                     .build();
         }
     }
@@ -84,24 +120,45 @@ public class HelpSelectController {
 
         private String contents;
 
-
-        @Builder(access = AccessLevel.PROTECTED)
-        private EtcSelectResponse(Long id, Long helpRegisterId, String title, String placeId, LocalDateTime start, LocalDateTime end, ProgressSelectResponse progressSelectResponse, Long reward, String contents) {
+        @Builder(access = AccessLevel.PRIVATE)
+        private EtcSelectResponse(Long id, Long helpRegisterId, String title, String placeId, LocalDateTime start, LocalDateTime end, ProgressWriteController.ProgressSelectResponse progressSelectResponse, Long reward, String contents) {
             super(id, helpRegisterId, title, placeId, start, end, progressSelectResponse, reward);
             this.contents = contents;
         }
 
-        public static EtcSelectResponse from(Etc etc) {
+        @Override
+        public boolean canHandle(Help help) {
+            return help instanceof Etc;
+        }
+
+        @Override
+        public EtcSelectResponse createResponse(Help help) {
+            Etc etc = (Etc) help;
             return EtcSelectResponse.builder()
                     .id(etc.getId())
-                    .title(etc.getTitle())
                     .helpRegisterId(etc.getHelpRegisterId())
+                    .title(etc.getTitle())
                     .placeId(etc.getPlaceId())
-                    .progressSelectResponse(ProgressSelectResponse.getProgressSelectResponse(etc.getProgress()))
                     .start(etc.getStart())
                     .end(etc.getEnd())
                     .reward(etc.getReward())
+                    .progressSelectResponse(ProgressWriteController.ProgressSelectResponse.getProgressSelectResponse(etc.getProgress()))
                     .contents(etc.getContents())
+                    .build();
+        }
+
+        //Test
+        public static EtcSelectResponse createForTest() {
+            return EtcSelectResponse.builder()
+                    .id(1L)
+                    .helpRegisterId(1L)
+                    .title("title")
+                    .placeId("placeId")
+                    .start(LocalDateTime.now())
+                    .end(LocalDateTime.now())
+                    .progressSelectResponse(ProgressWriteController.ProgressSelectResponse.getProgressSelectResponse(Progress.DEFAULT))
+                    .reward(100L)
+                    .contents("contents")
                     .build();
         }
     }
@@ -110,22 +167,43 @@ public class HelpSelectController {
     @NoArgsConstructor(access = AccessLevel.PROTECTED)
     public static class LineUpSelectResponse extends HelpSelectResponse {
 
-        @Builder(access = AccessLevel.PROTECTED)
-        private LineUpSelectResponse(Long id, Long helpRegisterId, String title, String placeId, LocalDateTime start, LocalDateTime end, ProgressSelectResponse progressSelectResponse, Long reward) {
+        @Builder(access = AccessLevel.PRIVATE)
+        private LineUpSelectResponse(Long id, Long helpRegisterId, String title, String placeId, LocalDateTime start, LocalDateTime end, ProgressWriteController.ProgressSelectResponse progressSelectResponse, Long reward) {
             super(id, helpRegisterId, title, placeId, start, end, progressSelectResponse, reward);
         }
 
-        public static LineUpSelectResponse from(LineUp lineUp) {
+        @Override
+        public boolean canHandle(Help help) {
+            return help instanceof LineUp;
+        }
+
+        @Override
+        public LineUpSelectResponse createResponse(Help help) {
             return LineUpSelectResponse.builder()
-                    .id(lineUp.getId())
-                    .helpRegisterId(lineUp.getHelpRegisterId())
-                    .title(lineUp.getTitle())
-                    .placeId(lineUp.getPlaceId())
-                    .start(lineUp.getStart())
-                    .end(lineUp.getEnd())
-                    .reward(lineUp.getReward())
-                    .progressSelectResponse(ProgressSelectResponse.getProgressSelectResponse(lineUp.getProgress()))
+                    .id(help.getId())
+                    .helpRegisterId(help.getHelpRegisterId())
+                    .title(help.getTitle())
+                    .placeId(help.getPlaceId())
+                    .start(help.getStart())
+                    .end(help.getEnd())
+                    .reward(help.getReward())
+                    .progressSelectResponse(ProgressWriteController.ProgressSelectResponse.getProgressSelectResponse(help.getProgress()))
+                    .build();
+        }
+
+        //Test
+        public static LineUpSelectResponse createForTest() {
+            return LineUpSelectResponse.builder()
+                    .id(1L)
+                    .helpRegisterId(1L)
+                    .title("title")
+                    .placeId("placeId")
+                    .start(LocalDateTime.now())
+                    .end(LocalDateTime.now())
+                    .progressSelectResponse(ProgressWriteController.ProgressSelectResponse.getProgressSelectResponse(Progress.DEFAULT))
+                    .reward(100L)
                     .build();
         }
     }
+
 }
