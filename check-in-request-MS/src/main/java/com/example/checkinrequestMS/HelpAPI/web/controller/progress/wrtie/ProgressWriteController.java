a460f9b3198476ae7e5b4ef1bf749e607129d94b
ScottSung7 <sungscott@naver.com>
2024-11-16 09:42:35 +0900

    [HOTFIX-11-4-Check-In-MS-표현계층-형식변경] ProgressWrite 관련 변경 파일

--- check-in-request-MS/src/main/java/com/example/checkinrequestMS/HelpAPI/web/controller/progress/wrtie/ProgressWriteController.java
+++ check-in-request-MS/src/main/java/com/example/checkinrequestMS/HelpAPI/web/controller/progress/wrtie/ProgressWriteController.java
@@ -2,9 +2,10 @@ package com.example.checkinrequestMS.HelpAPI.web.controller.progress.wrtie;
 
 import com.example.checkinrequestMS.HelpAPI.application.service.progress.write.ProgressWriteService;
 
-import com.example.checkinrequestMS.HelpAPI.web.controller.dto.request.progress.write.ProgressRegisterRequest;
-import com.example.checkinrequestMS.HelpAPI.web.controller.dto.response.progress.business.ProgressChangeResponse;
-import lombok.RequiredArgsConstructor;
+import com.example.checkinrequestMS.HelpAPI.domain.model.help.Progress;
+import com.example.checkinrequestMS.HelpAPI.web.controller.progress.business.ProgressBusinessWriteController;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
@@ -20,8 +21,46 @@ public class ProgressWriteController {
     private static String PICKED = "도와주실 분을 구했습니다.";
 
     @PostMapping
-    public ResponseEntity<ProgressChangeResponse> registerProgress(@Validated @RequestBody ProgressRegisterRequest form) {
+    public ResponseEntity<ProgressBusinessWriteController.ProgressChangeResponse> registerProgress(@Validated @RequestBody ProgressRegisterRequest form) {
         Long helpId = progressRegisterService.registerHelper(form.getHelpId(), form.getHelperId());
-        return ResponseEntity.ok(ProgressChangeResponse.of(helpId, PICKED));
+        return ResponseEntity.ok(ProgressBusinessWriteController.ProgressChangeResponse.of(helpId, PICKED));
     }
+
+    //Request
+    @Getter
+    @Builder(access = AccessLevel.PROTECTED)
+    @RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class ProgressRegisterRequest {
+
+        public static final String PROGRESS_REGISTER_REQUEST_NO_HELP_ID = "요청 ID가 필요합니다.";
+        public static final String PROGRESS_REGISTER_REQUEST_NO_HELPER_ID = "요청 도우미의 ID가 필요합니다.";
+
+        @NotNull(message = PROGRESS_REGISTER_REQUEST_NO_HELP_ID)
+        private final Long helpId;
+
+        @NotNull(message = PROGRESS_REGISTER_REQUEST_NO_HELPER_ID)
+        private final Long helperId;
+
+    }
+
+    //Response
+    @Getter
+    @Builder(access = AccessLevel.PROTECTED)
+    public static final class ProgressSelectResponse {
+
+        private final Progress.ProgressStatus status;
+        private final Long helperId;
+        private final String photoPath;
+        private final boolean completed;
+
+        public static ProgressSelectResponse getProgressSelectResponse(Progress progress) {
+            return ProgressSelectResponse.builder()
+                    .completed(progress.isCompleted())
+                    .helperId(progress.getHelperId().orElse(null))
+                    .photoPath(progress.getPhotoPath().orElse(null))
+                    .status(progress.getStatus())
+                    .build();
+        }
+    }
+
 }
