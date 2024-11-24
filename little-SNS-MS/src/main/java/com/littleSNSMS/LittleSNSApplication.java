4ddcae70bd39e6905b21974673a55e0da80f58e1
ScottSung7 <sungscott@naver.com>
2024-11-24 22:05:24 +0900

    [53-Little-SNS-생성] 리액티브 프로그래밍을 위해 Little SNS MS를 생성하였습니다.

---
+++ little-SNS-MS/src/main/java/com/littleSNSMS/LittleSNSApplication.java
@@ -0,0 +1,12 @@
+package com.littleSNSMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LittleSNSApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LittleSNSApplication.class, args);
+    }
+
+}
