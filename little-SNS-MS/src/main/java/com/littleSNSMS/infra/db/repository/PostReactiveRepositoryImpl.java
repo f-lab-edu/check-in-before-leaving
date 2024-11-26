a193c5acdd49c440b88184f08411d80d8636d5aa
ScottSung7 <sungscott@naver.com>
2024-11-26 16:16:54 +0900

    [Experiment-02-r2dbc-에서-리포지토리-관리] 주요 파일

--- little-SNS-MS/src/main/java/com/littleSNSMS/infra/db/repository/PostReactiveRepositoryImpl.java
+++ little-SNS-MS/src/main/java/com/littleSNSMS/infra/db/repository/PostReactiveRepositoryImpl.java
@@ -1,7 +1,9 @@
 package com.littleSNSMS.infra.db.repository;
 
+import com.littleSNSMS.domain.LikeMemberInfo;
 import com.littleSNSMS.domain.Post;
 import com.littleSNSMS.domain.PostReactiveRepository;
+import com.littleSNSMS.infra.db.entities.LikeMemberInfoEntity;
 import com.littleSNSMS.infra.db.entities.PostEntity;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -16,6 +18,20 @@ public class PostReactiveRepositoryImpl implements PostReactiveRepository {
     @Override
     public Mono<Long> save(Post post) {
         return postR2dbcRepository.save(PostEntity.post(post)).map(PostEntity::getPostId);
+    }
+
+    @Override
+    public Mono<Void> likePost(Long postId, LikeMemberInfo likeMemberInfo) {
+        postR2dbcRepository.likePost(postId, likeMemberInfo.getMemberEmail(), likeMemberInfo.getMemberId());
+        return Mono.empty(); //todo: 에러 처리.
+    }
+
+    public Mono<Post> findById(Long postId) {
+        return postR2dbcRepository.findById(postId).flatMap(postEntity -> {
+            return postR2dbcRepository.findLikeMemberInfo(postId).collectList().map(likeMemberInfoEntities -> {
+                return postEntity.toPost(likeMemberInfoEntities);
+            });
+        });
     }
 
 }
