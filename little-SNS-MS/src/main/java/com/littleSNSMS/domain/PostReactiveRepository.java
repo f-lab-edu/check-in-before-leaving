23e0aa769593fe309b5645312ac4a47a194bf1c4
ScottSung7 <sungscott@naver.com>
2024-11-26 16:18:44 +0900

    [Experiment-02-r2dbc-에서-리포지토리-관리] 영향 받은 파일

--- little-SNS-MS/src/main/java/com/littleSNSMS/domain/PostReactiveRepository.java
+++ little-SNS-MS/src/main/java/com/littleSNSMS/domain/PostReactiveRepository.java
@@ -1,7 +1,10 @@
 package com.littleSNSMS.domain;
 
+import com.littleSNSMS.domain.dto.PostDTO;
 import reactor.core.publisher.Mono;
 
 public interface PostReactiveRepository {
     Mono<Long> save(Post post);
+
+    Mono<Void> likePost(Long postId, LikeMemberInfo likeMemberInfo);
 }
