7bb922490f1eeb302d51cf80040db24077989f7f
ScottSung7 <sungscott@naver.com>
2024-12-03 14:54:00 +0900

    [Hotfix-18-상속-구조-변경-Save-시나리오] LineUp, Etc 인프라. Save 기능 프로덕션 코드.

---
+++ check-in-request-MS/src/main/java/com/example/checkinrequestMS/HelpAPI/infra/db/entity/child/LineUpEntity.java
@@ -0,0 +1,49 @@
+package com.example.checkinrequestMS.HelpAPI.infra.db.entity.child;
+
+import com.example.checkinrequestMS.HelpAPI.domain.model.help.child.LineUp;
+import com.example.checkinrequestMS.HelpAPI.infra.db.entity.HelpDetailEntity;
+import com.example.checkinrequestMS.HelpAPI.infra.db.entity.ProgressEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "line_up")
+public class LineUpEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "line_up", nullable = false)
+    private Long id;
+
+    @Embedded
+    private HelpDetailEntity helpEntity;
+
+    @Embedded
+    private ProgressEntity progressEntity;
+
+    @Builder(access = AccessLevel.PROTECTED)
+    protected LineUpEntity(@NonNull Long id, @NonNull LineUp lineUp) {
+        this.id = id;
+        this.helpEntity = HelpDetailEntity.toDB(lineUp.getHelpDetail());
+        this.progressEntity = ProgressEntity.from(lineUp.getProgress());
+    }
+
+    public static LineUpEntity toDB(LineUp lineUp) {
+        return LineUpEntity.builder()
+                .id(lineUp.getId())
+                .lineUp(lineUp)
+                .build();
+    }
+
+    //for test
+    public static LineUpEntity createForTest() {
+        return LineUpEntity.builder()
+                .id(1L)
+                .lineUp(LineUp.createForTest())
+                .build();
+    }
+
+
+}
---
+++ check-in-request-MS/src/main/java/com/example/checkinrequestMS/HelpAPI/infra/db/help/LineUpJPARepository.java
@@ -0,0 +1,27 @@
+package com.example.checkinrequestMS.HelpAPI.infra.db.help;
+
+import com.example.checkinrequestMS.HelpAPI.domain.exceptions.help.HelpErrorCode;
+import com.example.checkinrequestMS.HelpAPI.domain.exceptions.help.HelpException;
+import com.example.checkinrequestMS.HelpAPI.domain.model.help.child.LineUp;
+import com.example.checkinrequestMS.HelpAPI.domain.model.help.child.LineUpRepository;
+import com.example.checkinrequestMS.HelpAPI.infra.db.entity.child.LineUpEntity;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class LineUpJPARepository implements LineUpRepository {
+
+    private final LineUpSpringJPARepository lineUpSpringJPARepository;
+
+    @Override
+    public Long save(LineUp lineUp) {
+        return lineUpSpringJPARepository.save(LineUpEntity.toDB(lineUp)).getId();
+    }
+
+    @Override
+    public LineUp findById(Long id) {
+        return LineUp.toDomain(lineUpSpringJPARepository.findById(id)
+                .orElseThrow(() -> new HelpException(HelpErrorCode.NO_HELP_INFO)));
+    }
+}
