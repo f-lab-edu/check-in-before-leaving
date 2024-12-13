c14a2b12f4329a54783dbe6d94b570de72a0f992
ScottSung7 <sungscott@naver.com>
2024-12-13 22:01:10 +0900

    [63-메서드-시간제는-AOP-정상화] 이름 변경 및 정상화 확인, 테스트 컨테이너로 테스트 변경.

--- member-context/src/main/java/com/membercontext/memberAPI/application/aop/testTime/TestTime.java
+++ member-context/src/main/java/com/membercontext/memberAPI/application/aop/testTime/TestTime.java
@@ -1,4 +1,4 @@
-package com.membercontext.memberAPI.application.aop.log;
+package com.membercontext.memberAPI.application.aop.testTime;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -7,7 +7,7 @@ import java.lang.annotation.Target;
 
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
-public @interface Log {
+public @interface TestTime {
 
 
 }
