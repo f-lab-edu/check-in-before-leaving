21f21e7354a2a554d9bbf6e3498adcd17d61c171
ScottSung7 <sungscott@naver.com>
2025-02-20 17:05:42 +0900

    hotfix: 대용량 파일 테스트 불가로 임시 페이지 제작.

--- front-server/src/main/resources/static/js/file/fileUploadSequential.js
+++ front-server/src/main/resources/static/js/file/fileUploadSequential.js
@@ -1,5 +1,20 @@
+document.getElementById('uploadButton').addEventListener('click', () => {
+    const file = document.getElementById('fileInput').files[0];
+    if (file) {
+        uploadFile(file);
+    } else {
+        alert('파일을 선택해 주세요.');
+    }
+});
+
 const uploadFile = (file) => {
-    const chunkSize = 1024 * 1024; // 1MB
+
+    if (!file) {
+        alert('파일을 선택해 주세요.');
+        return;
+    }
+
+    const chunkSize = 200 * 1024 * 1024;
     const totalChunks = Math.ceil(file.size / chunkSize);
     let currentChunk = 0;
 
@@ -14,6 +29,7 @@ const uploadFile = (file) => {
         formData.append("totalChunks", totalChunks);
         formData.append("filename", file.name);
 
+        //fixme: 성능 향상위한 부분 재전송 로직 추가하기.
         fetch("http://localhost:8086/upload/chunk", {
             method: "POST",
             body: formData
@@ -35,4 +51,6 @@ const uploadFile = (file) => {
 
     sendNextChunk();
 };
-module.exports = {uploadFile};
+
+//check: module 있으면 node.js 로직으로 인식해서 import가 불가. -> 추후 공존 법 모색.
+//module.exports = {uploadFile};
